072b24c7b6205b8b246f94184cf7e465683f9fcd
Lindsey Sandberg <sandbergy@yahoo.com>
2019-03-11 09:18:49 -0500

    changed wording/comments

--- server/Routes/project.router.js
+++ server/Routes/project.router.js
@@ -12,7 +12,7 @@ router.get('/api/project', (req, res) => {
             console.log(result) 
             res.send(result.rows); })
         .catch((err) => {
-            console.log('Error completing SELECT plant query', err);
+            console.log('Error completing SELECT Project query', err);
             res.sendStatus(500);
         });
 });
@@ -42,37 +42,6 @@ VALUES ($1, $2, $3, $4, $5, $6, $7);
         });
 });
 
-// router.put('/', (req, res) => {
-//     const updatedPlant = req.body;
-
-//     const queryText = `UPDATE table_name
-//   SET "name" = $1, 
-//   "kingdom" = $2, 
-//   "clade" = $3, 
-//   "order" = $4, 
-//   "family" = $5, 
-//   "subfamily" = $6, 
-//   "genus" = $7
-//   WHERE id=$8;`;
-
-//     const queryValues = [
-//         updatedPlant.name,
-//         updatedPlant.kingdom,
-//         updatedPlant.clade,
-//         updatedPlant.order,
-//         updatedPlant.family,
-//         updatedPlant.subfamily,
-//         updatedPlant.genus,
-//         updatedPlant.id,
-//     ];
-
-//     pool.query(queryText, queryValues)
-//         .then(() => { res.sendStatus(200); })
-//         .catch((err) => {
-//             console.log('Error completing SELECT plant query', err);
-//             res.sendStatus(500);
-//         });
-// });
 
 router.delete('/:id', (req, res) => {
     const queryText = 'DELETE FROM project WHERE id=$1';
